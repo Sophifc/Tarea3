4957bfdff8341fa9cbde8220d462bef97accf2fc
Sophifc <88857873+Sophifc@users.noreply.github.com>
2022-05-17 15:37:34 -0400

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+
+
+void initMenu(){
+
+
+    int eleccion;
+
+    while(eleccion!=0){
+
+        printf("Opciones del menu: \n");
+        printf("0-. Salir del menu\n");
+        printf("1-. Cargar documentos\n");
+        printf("2-. Mostrar documentos ordenaods\n");
+        printf("3-. Buscar libro por titulo\n");
+        printf("4-. Palabras con mayor fecuencia\n");
+        printf("5-. Palabras mas relevants\n");
+        printf("6-. Buscar por palabra");
+        printf("7-. Mostrar palabra en un contexto");
+
+        scanf("%d",&eleccion);
+        
+
+        switch (eleccion){
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+
+        case 3:
+
+          break;
+
+        case 4:
+
+           break;
+
+        case 5:
+
+          break;
+
+        case 6:
+
+           break;
+
+        case 7:
+
+          break;
+
+        }
+    }
+
+
+}
